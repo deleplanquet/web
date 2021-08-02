5c0e8dd199a97609b427b68da0da7c05ec376baf
fld46 <fredimp@orange.fr>
2021-08-02 13:55:35 +0200

    routeur

--- controllers/Router.php
+++ controllers/Router.php
@@ -3,4 +3,47 @@ class Router
 {
     private $_ctrl;
     private $_view;
+
+    public function routeReq()
+    {
+        try
+        {
+            // CHARGEMENT AUTOMATIQUE DES CLASSES
+            spl_autoload_register(function($class){
+                require_once($_SERVER["DOCUMENT_ROOT"]."/models/".$class.'.php');
+            });
+
+            $url = '';
+
+            // LE CONTROLLER EST INCLUS SELON L'ACTION DE L'UTILISATEUR
+            if(isset($_GET['url']))
+            {
+                $url = explode('/', filter_var($_GET['url'],FILTER_SANITIZE_URL));
+                $controller = ucfirst(strtolower($url[0]));
+                $controllerClass = "Controller".$controller;
+                $controllerFile = "controllers/".$controllerClass.".php";
+
+                if(file_exists($controllerFile))
+                {
+                    require_once($controllerFile);
+                    $this->_ctrl = new $controllerClass($url);
+                }
+                else
+                    throw new Exception('Page introuvable');
+            
+                
+            }
+            else
+            {
+                require_once('controllers/ControllerAccueil.php');
+                $this->_ctrl = new ControllerAccueil($url);    
+            }
+        }
+        // GESTION DES ERREURS
+        catch(Exception $e)
+        {
+            $errorMsg = $e->getMessage();
+            require_once('views/viewError.php');            
+        }
+    }
 }
